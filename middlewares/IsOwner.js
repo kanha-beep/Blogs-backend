fedc8409e2f72712c505567167e08237c94478e5
Your Name <you@example.com>
2026-03-20 19:49:58 +0530

    blog

--- middlewares/IsOwner.js
+++ middlewares/IsOwner.js
@@ -4,7 +4,7 @@ export const isOwner = async (req, res, next) => {
     console.log("user owner: ", req.user)
     const blog = await Blog.findById(req.params.id).populate("user")
     if (!blog) return next(new ExpressError(404, "Blog not found"));
-    if (!blog.user_id.string() === req.user._id.string()) return next(new ExpressError(404, "String Not authorized"))
+    if (blog.user_id.string() !== req.user._id.string()) return next(new ExpressError(404, "String Not authorized"))
     if (!blog.user._id.equals(req.user._id)) return next(new ExpressError(404, "Not authorized"))
     console.log("current user: ", req.user._id);
     console.log("blog owner: ", blog.user._id)
